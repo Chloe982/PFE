8f4aaaf6c770ebcae25b20c108ffb71cf0835780
Chloe <chloe.marce@efrei.net>
2022-03-24 10:02:29 +0100

    canva + scripts

---
+++ PremierSecours/Assets/Scripts/teleport.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class teleport : MonoBehaviour
+{
+
+    public GameObject origin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //origin = FindObjectOfType<XROrigin>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void teleportTo()
+    {
+        origin.transform.position = new Vector3(-4f, 0f, 0f);
+    }
+}
