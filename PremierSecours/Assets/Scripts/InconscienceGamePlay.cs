67da1b33b973f83f94db35e6c0a9e9736446c941
Chloe <chloe.marce@efrei.net>
2022-03-26 12:43:09 +0100

    Ajout PLS + Canvas et anims du choix 3

--- PremierSecours/Assets/Scripts/InconscienceGamePlay.cs
+++ PremierSecours/Assets/Scripts/InconscienceGamePlay.cs
@@ -26,6 +26,8 @@ public class InconscienceGamePlay : MonoBehaviour
         
     }
 
+    //CHOIX 1
+
     public void Reaction()
     {
         Vector3 position = new Vector3(-6f, 0.2f, 1.5f);
@@ -33,7 +35,6 @@ public class InconscienceGamePlay : MonoBehaviour
         //desactiver les 3 canvas et activer le suivant
         string flag = "reaction";
         StartCoroutine(Wait(flag));
-        //Wait();
         
     }
 
@@ -51,6 +52,8 @@ public class InconscienceGamePlay : MonoBehaviour
         //explication 
     }
 
+    //CHOIX2 
+
     public void Respire()
     {
         Vector3 position = new Vector3(-5.7f, 0.7f, 1.5f);
@@ -58,7 +61,6 @@ public class InconscienceGamePlay : MonoBehaviour
         //desactiver les 3 canvas et activer le suivant
         string flag = "respire";
         StartCoroutine(Wait(flag));
-        //Wait();
 
     }
 
@@ -76,6 +78,35 @@ public class InconscienceGamePlay : MonoBehaviour
         //explication 
     }
 
+    //CHOIX 3
+
+    public void Pls()
+    {
+        Vector3 position = new Vector3(-4.2f, 0.7f, 1.5f);
+        AnimVerte(position);
+        
+        //string flag = "pls";
+        //StartCoroutine(Wait(flag));
+        //Wait();
+
+    }
+
+    public void Pos1()
+    {
+        Vector3 position = new Vector3(-6.8f, 0.67f, -0.28f);
+        AnimRouge(position);
+        //explication 
+    }
+
+    public void Pos2()
+    {
+        Vector3 position = new Vector3(-5.0f, 1.46f, -1.9f);
+        AnimRouge(position);
+        //explication 
+    }
+
+    //ANIMATIONS
+
     public void AnimVerte(Vector3 position)
     {
         GameObject feuVert = Instantiate(animVerte, position, Quaternion.identity) as GameObject;
@@ -89,6 +120,8 @@ public class InconscienceGamePlay : MonoBehaviour
         Destroy(feuRouge, 3);
     }
 
+    //WAIT
+
     IEnumerator Wait(string flag)
     {
         
@@ -98,13 +131,14 @@ public class InconscienceGamePlay : MonoBehaviour
         
     }
 
+    //CHANGEMENT DE CHOIX
+
     public void DiscardObjects(string flag)
     {
         if(flag == "reaction")
         {
             
             choix1.gameObject.SetActive(false);
-
             choix2.gameObject.SetActive(true);
             
         }
